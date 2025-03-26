a4a9232173b767fccb568994be63b19d8f75b031
Arutyunyan Rafael <raymix.yutuber.2017@gmail.com>
2025-03-26 18:56:58 +0300

    LB1.T1.Add Church list implementation in F#
    - Implemented ChurchList type for Church lists representation
    - Added fromList and toList conversion functions
    - Implemented readChurchList function to read list from input
    - Added simple demo program to show functionality

---
+++ lr6-List/Program.fs
@@ -0,0 +1,12 @@
+﻿module Program
+
+open Task1
+
+[<EntryPoint>]
+let main argv =
+    printf "Введите количество элементов: "
+    let n = int (System.Console.ReadLine())
+    let churchList = readChurchList n
+    let normalList = toList churchList
+    printfn "Список: %A" normalList
+    0
---
+++ lr6-List/Task.fs
@@ -0,0 +1,19 @@
+﻿module Task1
+
+// Определение списка Черча
+type ChurchList<'T> = ChurchList of (('T -> 'T list -> 'T list) -> 'T list -> 'T list)
+
+// Функция создания списка Черча из стандартного списка
+let fromList lst =
+    ChurchList (fun f acc -> List.foldBack f lst acc)
+
+// Функция преобразования списка Черча в обычный список
+let toList (ChurchList churchList) =
+    churchList (fun x acc -> x :: acc) []
+
+// Функция чтения списка чисел из ввода пользователя
+let readChurchList n =
+    let rec readHelper acc count =
+        if count = 0 then acc
+        else readHelper (int (System.Console.ReadLine()) :: acc) (count - 1)
+    fromList (List.rev (readHelper [] n))
