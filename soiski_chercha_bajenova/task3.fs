ede5d954dbd585b49b159de3e43c4aa7b057ae14
Arutyunyan Rafael <raymix.yutuber.2017@gmail.com>
2025-03-26 15:04:14 +0300

    Book Bajenov-spiski

---
+++ soiski_chercha_bajenova/task3.fs
@@ -0,0 +1,9 @@
+﻿module task3
+
+let salaries = [10.0; 11.0; 12.5; 15.0; 13.0; 7.5]
+let increaseRate = 1.15
+
+let newSalaries = salaries |> List.map (fun salary -> salary * increaseRate)
+
+let run () =
+    printfn "Новые оклады сотрудников: %A" newSalaries
---
+++ soiski_chercha_bajenova/task5.fs
@@ -0,0 +1,12 @@
+﻿module task5
+
+
+let numbers = [1..20]
+
+// Отбираем чётные числа и возводим их в квадрат
+let evenSquares = numbers |> List.filter (fun x -> x % 2 = 0) |> List.map (fun x -> x * x)
+
+let run () =
+    printfn "Квадраты чётных чисел из 1-20: %A" evenSquares
+
+
