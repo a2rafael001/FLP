96eed53def18412acef1fde6845921e667f98c9b
Arutyunyan Rafael <raymix.yutuber.2017@gmail.com>
2025-04-11 22:08:41 +0300

    task 4

---
+++ lr8/familyy.pl
@@ -0,0 +1,32 @@
+% ---------- Пол ----------
+man(ivan).
+man(alexey).
+man(sergey).
+man(nikolay).
+man(mikhail).
+man(daniil).
+man(andrey).
+
+woman(maria).
+woman(svetlana).
+woman(olga).
+woman(katya).
+
+% ---------- Родители ----------
+parent(ivan, alexey).
+parent(maria, alexey).
+
+parent(ivan, olga).
+parent(maria, olga).
+
+parent(ivan, sergey).
+parent(maria, sergey).
+
+parent(alexey, daniil).
+parent(svetlana, daniil).
+
+parent(alexey, katya).
+parent(svetlana, katya).
+
+parent(olga, andrey).
+parent(mikhail, andrey).
